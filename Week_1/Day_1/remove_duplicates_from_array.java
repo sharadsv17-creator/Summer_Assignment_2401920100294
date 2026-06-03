35c2ff05931e3abeba42b0620cbe96e3680eb5ad
SHARAD <sharadsv17@gmail.com>
2026-06-03 09:20:32 +0530

    Implement method to remove duplicates from array

--- Week_1/Day_1/remove_duplicates_from_array.java
+++ Week_1/Day_1/remove_duplicates_from_array.java
@@ -1,1 +1,11 @@
-
+public class remove_duplicates_from_array {
+    public int removeDuplicates(int[] nums) {
+        int k=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[k]!=nums[i]){
+                nums[++k]=nums[i];
+            }
+             } 
+        return k+1;   
+    }
+}
