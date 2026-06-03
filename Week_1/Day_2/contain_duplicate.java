991597f27639164c9bbcd2d284523723d5da03af
SHARAD <sharadsv17@gmail.com>
2026-06-03 09:36:13 +0530

    Implement containsDuplicate method in Solution class

--- Week_1/Day_2/contain_duplicate.java
+++ Week_1/Day_2/contain_duplicate.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      HashSet<Integer> m=new HashSet<>();
+        for (int i=0;i<nums.length;i++){
+            if (m.contains(nums[i])){
+                return true;
+            }
+            else {
+                m.add(nums[i]);
+            }
+        }
+     return false;     
+    }
+}
