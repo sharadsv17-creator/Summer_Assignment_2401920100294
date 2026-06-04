74608af9ae4b385806c524a7dfd7f33d36b56839
SHARAD <sharadsv17@gmail.com>
2026-06-04 10:17:22 +0530

    Day 4 solutions

--- Week_1/Day_4/Matrix_diagonal_sum.java
+++ Week_1/Day_4/Matrix_diagonal_sum.java
@@ -1,1 +1,16 @@
+class matrix_diagonal_sum {
+    public int diagonalSum(int[][] mat) {
+       int n=mat.length;
+        int sum=0;
 
+        for(int i= 0; i < n; i++) {
+            sum+=mat[i][i];
+            sum+=mat[i][n-1-i];
+        }
+        if (n%2==1) {
+            sum-=mat[n/2][n/2];
+        }
+
+        return sum;  
+    }
+}
