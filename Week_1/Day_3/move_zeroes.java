533772c02b607e53e6dd5e776099b3bad4486ade
SHARAD <sharadsv17@gmail.com>
2026-06-04 10:04:42 +0530

    Day 3 solutions
    Implement method to move all zeroes to the end of the array.

--- Week_1/Day_3/move_zeroes.java
+++ Week_1/Day_3/move_zeroes.java
@@ -1,1 +1,16 @@
-
+public class MoveZeroes {
+     public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int p = 0;
+        while(i < n) {
+            if(nums[i] != 0) {
+                nums[p++] = nums[i];
+            }
+            i++;
+        }
+        for(int k=p; k<n; k++) {
+            nums[k] = 0;
+        }
+    }
+}
