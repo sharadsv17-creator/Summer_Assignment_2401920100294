f3f5fe61c4624fe7fa0f1afa1d4e159cdf957c40
SHARAD <sharadsv17@gmail.com>
2026-06-05 23:30:24 +0530

    Day 5 solutions

--- Week_1/Day_5/validpalindrome.java
+++ Week_1/Day_5/validpalindrome.java
@@ -1,1 +1,23 @@
-
+public class ValidPalindrome {
+     public boolean isPalindrome(String s) {
+        String str = s.toLowerCase();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0; i<s.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9') {
+                sb.append(ch);
+            }
+        }
+        
+        int f = 0;
+        int l = sb.length()-1;
+        while(f <= l) {
+            if(sb.charAt(f) != sb.charAt(l)) {
+                return false;
+            }
+            f++;
+            l--;
+        }
+        return true;
+    }
+}
