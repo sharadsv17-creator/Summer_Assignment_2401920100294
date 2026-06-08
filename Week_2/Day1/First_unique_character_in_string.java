77d3bbcc28c6134376fb68b023ccb4824dbdccce
SHARAD <sharadsv17@gmail.com>
2026-06-08 21:54:00 +0530

    Week_2  Day_1 solutions

---
+++ Week_2/Day1/First_unique_character_in_string.java
@@ -0,0 +1,15 @@
+public class FirstUniqueCharacterInAString {
+     public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        int n = s.length();
+        for(int i=0; i<n; i++) {
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<n; i++) {
+            if(freq[s.charAt(i)-'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
