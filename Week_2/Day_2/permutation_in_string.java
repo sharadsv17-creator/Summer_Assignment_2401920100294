e8505d8e92d7b1958863e904c437b8cd93791fa4
SHARAD <sharadsv17@gmail.com>
2026-06-09 22:29:35 +0530

    Week_2  Day_2 solutions

---
+++ Week_2/Day_2/permutation_in_string.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class permutation_in_string {
+    public boolean checkInclusion(String s1, String s2) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        if(l1 > l2) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for(int i = 0; i < l1; i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if(Arrays.equals(freq1, freq2)) return true;
+
+        for(int i = l1; i < l2; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i - l1) - 'a']--;
+
+            if(Arrays.equals(freq1, freq2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
