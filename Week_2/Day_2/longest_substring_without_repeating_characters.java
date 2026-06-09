734beaa079545ab6e0d5b1b8a84fcbdd2624328e
SHARAD <sharadsv17@gmail.com>
2026-06-09 22:27:28 +0530

    Week_2  Day_2 solutions
    Implement method to find length of longest substring without repeating characters.

--- Week_2/Day_2/longest_substring_without_repeating_characters.java
+++ Week_2/Day_2/longest_substring_without_repeating_characters.java
@@ -1,1 +1,13 @@
-
+public class longest_substring_without_repeating_character {
+    public int lengthOfLongestSubstring(String s) {
+        char[] ch = s.toCharArray();
+        for(int i = 0; i < ch.length ; i++){
+            for(int j = 1 ; j < ch.length;j++){
+                if(ch[i] == ch[j]){
+                    return j;
+                }
+            }
+        }
+        return -1;
+    }
+}
