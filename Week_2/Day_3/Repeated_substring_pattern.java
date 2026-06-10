31ff8634b99045253bd1ffd24a841b5787159bbb
SHARAD <sharadsv17@gmail.com>
2026-06-10 22:31:30 +0530

    Week_2  Day_3 solutions

---
+++ Week_2/Day_3/Repeated_substring_pattern.java
@@ -0,0 +1,10 @@
+//Time Complexity O(n²) 
+//Space Complexity O(n)
+
+public class RepeatedSubstringPattern {
+     public boolean repeatedSubstringPattern(String s) {
+        String d = s + s;
+        String str = d.substring(1, d.length()-1);
+        return str.contains(s);
+    }
+}
