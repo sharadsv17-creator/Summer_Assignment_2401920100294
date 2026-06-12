f40bb8e86a7474d08d35796e53a90acb84e719db
SHARAD <sharadsv17@gmail.com>
2026-06-12 22:16:14 +0530

    Week_2  Day_5 solutions
    Implement string compression algorithm that modifies the input character array in place and returns the new length.

---
+++ Week_2/Day_5/String_compression.java
@@ -0,0 +1,37 @@
+/* TC: O(N)
+   SC: O(1) */
+
+
+import java.util.Arrays;
+public class StringCompression {
+ public int compress(char[] chars) {
+        int c = 1;
+        int j = 1;
+        for(int i=0; i<chars.length-1; i++) {
+           if(chars[i] == chars[i+1]) {
+               c++;
+           }else{
+              if(c != 1) {
+                String s  = Integer.toString(c);
+                for(int k=0; k<s.length(); k++) {
+                    chars[j] = s.charAt(k);
+                    j++;
+                }
+            
+              }
+              c = 1;
+              chars[j] = chars[i+1];
+              j++;
+           }
+        }
+        if(c != 1) {
+                String s  = Integer.toString(c);
+                for(int k=0; k<s.length(); k++) {
+                    chars[j] = s.charAt(k);
+                    j++;
+                }
+            
+              }
+        return j;
+    }
+}
